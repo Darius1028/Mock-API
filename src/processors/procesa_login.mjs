ec8e50aa75498d538d697a0884a3b54f50e6d9bd
Dario Morales <dariodbml@gmail.com>
2025-12-08 09:16:41 -0500

    ajuste de servicio api mock

--- src/processors/procesa_login.mjs
+++ src/processors/procesa_login.mjs
@@ -32,7 +32,7 @@ const uno = {
     "data": [
         {
             "identificador": "1718050709",
-            "perfiles": "2",
+            "perfiles": "9",
             "nombre": "BERNARDO DARIO",
             "apellido": "MORALES LOPEZ",
             "id_localizacion_provincia": 259,
@@ -74,7 +74,7 @@ const dos = {
     "data": [
         {
             "identificador": null,
-            "perfiles": "2,9",
+            "perfiles": "9",
             "nombre": null,
             "apellido": null,
             "id_localizacion_provincia": null,
